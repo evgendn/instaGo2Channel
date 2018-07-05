8228a1816ec4bca693ea47af815c0ce49e62ad36
evgendn <evgeniydein@gmail.com>
2018-07-06 00:37:34 +0500

    Pick original stories

--- src/stories.go
+++ src/stories.go
@@ -7,7 +7,6 @@ import (
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 	"strings"
-	// "fmt"
 )
 
 func getStories(username string) []string {
@@ -39,20 +38,21 @@ func fetchStoriesPage(username string) string {
 }
 
 func parseFromHTML(htmlPage string) []string {
-	var storiesLinks []string
+	var linksFromPage []string
 
 	tokenizer := html.NewTokenizer(strings.NewReader(htmlPage))
 	for {
 		tokenType := tokenizer.Next()
 		switch {
 		case tokenType == html.ErrorToken:
-			return storiesLinks
+			var storiesLinks  = findOriginalStories(&linksFromPage)
+			return storiesLinks 
 		case tokenType == html.StartTagToken:
 			token := tokenizer.Token()
 			if token.DataAtom == atom.A {
 				for _, attr := range token.Attr {
 					if attr.Key == "href" {
-						clearURLAndInsert(&attr.Val, &storiesLinks)
+						clearURLAndInsert(&attr.Val, &linksFromPage)
 					}
 				}
 			}
@@ -60,9 +60,35 @@ func parseFromHTML(htmlPage string) []string {
 	}	
 }
 
+
 func clearURLAndInsert(rawURL *string, slice *[]string) {
 	if strings.HasPrefix(*rawURL, "https") {
 		url := strings.Split(*rawURL,  "?")[0]
 		*slice = append(*slice, url)
 	}
 }
+
+func findOriginalStories(urls *[]string) []string {
+	var result []string
+	if len(*urls) == 1 {
+		result = append(result, (*urls)[0])
+	}
+	for i := 0; i < len(*urls) - 1; i++ {
+		// Scary fucking shit.
+		index := tryFindVideoIndex(&((*urls)[i]), &((*urls)[i + 1]), &i)
+		result = append(result, (*urls)[index])
+	}
+	return result
+}
+
+// Video has preview above as jpg format.
+// First checking two adjacent items of slice and change index by 3 because 
+// site that parsed has 3 videos different resolution for one stories. 
+func tryFindVideoIndex(url1, url2 *string, i *int) int {
+	j := *i
+	if strings.HasSuffix(*url1, "jpg") && strings.HasSuffix(*url2, "mp4") {
+		j++
+		*i += 3
+	}
+	return j
+} 
